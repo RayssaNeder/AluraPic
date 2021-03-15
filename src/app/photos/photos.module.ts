f207800d08f0bc9a48e5f03ac7a51ea1669c8a46
Rayssa Neder <rayssafreitas13@gmail.com>
2021-03-14 23:51:53 -0300

    including Font-Awesome

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,14 +1,18 @@
-
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { PhotoModule } from './photo/photo.module';
-import { PhotoListModule } from './photo-list/photo-list.module';
-import { PhotoFormModule } from './photo-form/photo-form.module';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ PhotoComponent, PhotoListComponent, PhotosComponent, FilterByDescription, LoadButtonComponent ],
   exports: [  ],
-  imports: [ PhotoModule, PhotoFormModule, PhotoListModule]
+  imports: [ HttpClientModule,  //Esse é o HttpCleint provider. É necessário para que o Angular consiga realizar a injeção de dependência do HTTPClient app.component.ts
+              CommonModule]
 
 })
 export class PhotosModule {}
